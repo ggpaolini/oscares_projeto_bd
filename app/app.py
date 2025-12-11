b13bfba1b02e362793516ea162069df2797bf7e0
FilipeZheng <zhengfilipe@gmail.com>
2025-12-11 12:24:08 +0000

    Avanços

--- app/app.py
+++ app/app.py
@@ -44,7 +44,7 @@ select * from ceremony where ceremonyNumber = ?
 select * from nomination where ceremonyNumber=?
 ''', [number]
     ).fetchall()
-    return render_template('mostrar_cerimonias', cerimonia=cerimonia, nomeacoes=nomeacoes)
+    return render_template('mostrar_cerimonias.html', cerimonia=cerimonia, nomeacoes=nomeacoes)
 
 @APP.route('/classes/')
 def list_classes():
@@ -55,21 +55,23 @@ SELECT * FROM CLASS ORDER BY ClassName
     ).fetchall()
     return render_template('listar_classes.html', classes=classes)
 
-@APP.route('/classes/<string:name>')
+@APP.route('/classes/<string:name>/')
 def show_classes(name):
+    t = name.lower()      # Tentei evitar algumas injeções aqui
+    if any(i in t for i in ("join","--","drop","table")): raise Exception("Nice Try")  #Checking for injections
     classe = db.execute(
+        f'''
+        select * from class where classname='{name}'
         '''
-select * from class where classname=?
-''', [name]
-    ).fetchone()
+        ).fetchone()
 
     categorias= db.execute(
+        f'''
+        select * from category
+        where classname = '{name}'
         '''
-select * from category
-where classname = ?
-''', [name]
-    ).fetchall()
-    return render_template('mostrar_classes', classe=classe, categorias=categorias)
+        ).fetchall()
+    return render_template('mostrar_classes.html', classe=classe, categorias=categorias)
 
 @APP.route('/categorias/')
 def list_categorias():
@@ -80,6 +82,24 @@ SELECT * FROM CATEGORY ORDER BY ClassName, CanonicalCategory
     ).fetchall()
     return render_template('listar_categorias.html', categorias=categorias)
 
+@APP.route('/categorias/<string:name>/')
+def show_categorias(name):
+    t = name.lower()      # Tentei evitar algumas injeções aqui
+    if any(i in t for i in ("join","--","drop","table")): raise Exception("Nice Try")  #Checking for injections
+    name = name.replace("%20"," ")
+    categoria = db.execute(
+        f'''
+        SELECT * FROM CATEGORY WHERE CategoryName = '{name}'
+        '''
+    ).fetchone()
+    nomeacoes = db.execute(
+        f'''
+        SELECT * FROM nomination WHERE CategoryName = '{name}'
+        '''
+        )
+    return render_template('mostrar_categorias.html', categoria=categoria,nomeacoes=nomeacoes)
+
+
 @APP.route('/filmes/')
 def list_filmes():
     filmes = db.execute(
@@ -89,7 +109,37 @@ SELECT * FROM FILM ORDER BY FilmName
     ).fetchall()
     return render_template('listar_filmes.html', filmes=filmes)
 
-#@APP.route('/filmes/<int:id')
+@APP.route('/filmes/<string:id_name>')
+def show_filmes(id_name):
+    t = id_name.lower()      # Tentei evitar algumas injeções aqui
+    if any(i in t for i in ("join","--","drop","table")): raise Exception("Nice Try")  #Checking for injections
+    id,name = id_name.split("_")
+    name = name.replace("%20"," ").replace("'","''")
+    filme = db.execute(
+        f'''
+    SELECT * FROM FILM WHERE FilmId = '{id}' and FilmName = '{name}'
+    '''
+    ).fetchone()
+    nomeacoes = db.execute(
+        f'''
+        select * from nomination where NomId in 
+        (
+        select nomid from nominee
+        natural join nomination_film
+        WHERE FilmId = '{id}' and FilmName = '{name}'
+        )
+        '''
+        ).fetchall()
+    nomeados = db.execute(
+        f'''
+        select distinct n.* FROM nomination
+        natural join nomination_film
+        natural join nomination_nominee
+        natural join nominee n
+        WHERE FilmId = '{id}' and FilmName = '{name}'
+        '''
+        ).fetchall()
+    return render_template('mostrar_filmes.html', filme=filme,nomeacoes=nomeacoes,nomeados=nomeados)
 
 @APP.route('/nomeados/')
 def list_nomeados():
@@ -139,12 +189,34 @@ def show_nomeado(id):
 def list_nomeacoes():
     nomeacoes = db.execute(
         '''
-SELECT * FROM NOMINATION ORDER BY CeremonyNumber, ClassName, CanonicalCategory, NomId
-'''
+    SELECT * FROM NOMINATION ORDER BY CeremonyNumber, ClassName, CategoryName, NomId
+    '''
     ).fetchall()
     return render_template('listar_nomeacoes.html', nomeacoes=nomeacoes)
 
-#@APP.route('/nomeacoes/<int:nomid>')
-#def show_nomeacoes(id):
+@APP.route('/nomeacoes/<int:nomid>/')
+def show_nomeacoes(nomid):
+    nomeacao = db.execute('''
+        SELECT * FROM NOMINATION where NomId = ?
+        '''
+        ,[nomid]).fetchone()
+    filmes = db.execute(
+        '''
+        select f.* FROM nomination
+        natural join nomination_film f
+        where NomId = ?
+        '''
+        ,[nomid]).fetchall()
+    nomeados = db.execute(
+        '''
+        select n.* FROM nomination
+        natural join nomination_nominee
+        natural join nominee n
+        where NomId = ?
+        '''
+        ,[nomid]).fetchall()
+    return render_template('mostrar_nomeacoes.html',nomeacao=nomeacao,filmes=filmes,nomeados=nomeados)
+    
+
 #ainda nao sei o que meter aqui
 
